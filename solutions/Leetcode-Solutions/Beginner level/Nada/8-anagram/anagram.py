a659ce2d4ad38a1442d05c2bdd0787c2343b1e08
nada <excellence.nadamaher@gmail.com>
2024-03-24 20:14:53 +0200

    new solutions

---
+++ solutions/Leetcode-Solutions/Beginner level/Nada/8-anagram/anagram.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # Check if lengths are equal
+        if len(s) != len(t):
+            return False
+        
+        s_count = {}
+        t_count = {}
+        
+        for char in s:
+            s_count[char]=s_count.get(char,0) + 1
+        for char in t:
+            t_count[char]=t_count.get(char, 0) +1
+        return s_count == t_count            
