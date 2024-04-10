0b6ece7d7d6684ccbe26ea8a55751e9ad600ac18
youmna16 <elshennawiyou@gmail.com>
2024-04-10 04:25:13 +0200

    Added isomorphic strings solution

---
+++ solutions/Leetcode-Solutions/Beginner level/Youmna/12. isomorphic strings/sol.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        if len(set(s)) != len(set(t)):
+            return False
+        dic={}
+        for i in range(len(s)):
+            if s[i] not in dic:
+                dic[s[i]] = t[i]
+            elif dic[s[i]] !=t[i]:
+                return False
+        return True  
