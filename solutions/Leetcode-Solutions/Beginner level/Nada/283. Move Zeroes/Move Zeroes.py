79c46b1d3fab3cae3256500b4b944372000eea0d
nada <excellence.nadamaher@gmail.com>
2024-05-04 02:38:00 +0300

    New solutions :)

---
+++ solutions/Leetcode-Solutions/Beginner level/Nada/283. Move Zeroes/Move Zeroes.py
@@ -0,0 +1,10 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        l = 0
+        for r in range (len (nums)):
+            if nums[r]:
+                nums[l] , nums[r] = nums[r] , nums[l]
+                l += 1
+        return nums 
+        
+        
